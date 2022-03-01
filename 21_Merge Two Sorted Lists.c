9cab36f90a94743ac480273257f034c6ffc7d030
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-03-01 22:18:22 +0800

    Update 21_Merge Two Sorted Lists.c

--- 21_Merge Two Sorted Lists.c
+++ 21_Merge Two Sorted Lists.c
@@ -6,7 +6,7 @@
  * };
  */
 
-
+//iterative
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
     
     struct ListNode *head=NULL, *ptr1, *ptr2, *store,* store2; //store:the node to be add;store2:traverse ne linked list
@@ -54,3 +54,30 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
     
     return head;    
 }
+
+
+
+//recursive
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+    
+    if(!list1)
+        return list2;
+    if(!list2)
+        return list1;
+    if(list1->val<list2->val)
+    {
+        list1->next=mergeTwoLists(list1->next,list2);
+        return list1;
+    }
+    else
+    {
+        list2->next=mergeTwoLists(list1,list2->next);
+        return list2;   
+    }
+    
+}
+
+
+
+
+
