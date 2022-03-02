eac1307ff0b435e02f15324dd667b55128c7a691
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-03-02 22:10:22 +0800

    Update 69_Sqrt(x).c

--- 69_Sqrt(x).c
+++ 69_Sqrt(x).c
@@ -16,20 +16,20 @@ int mySqrt(int x){
 
 //***binary search template***
 
-// range   [l,r)
+// range   [l,r) left close right open
 // function binarysearch(l,r)
 // {
 //   while(l<r)
 //   {
-//     m=l+(r-l)/2
+//     m=l+(r-l)/2 same as m=(l+r)/2
 //     if f(m) return m (optional)
-//     if g(m) return
+//     if g(m) 
 //       r=m
 //     else
 //       l=m+1
 //   }
 //   return l or not found(if f() exist)
-//   l will be smallest num that g() can exist after execute binary search  (given a function g(mid),returns the smallest mid in the range such that g(mid) holds true)
+//   l will equal to r after while(because loop exist if l<r),and  will be smallest index that g() be true after execute binary search  (given a function g(mid),returns the smallest mid in the range such that g(mid) holds true)
 // }
 
 //binary search
