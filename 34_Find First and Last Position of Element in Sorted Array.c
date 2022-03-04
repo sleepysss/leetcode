8fe0677d6218bc23e4689f640fc694725b64c54e
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-03-05 01:03:00 +0800

    Update 34_Find First and Last Position of Element in Sorted Array.c

--- 34_Find First and Last Position of Element in Sorted Array.c
+++ 34_Find First and Last Position of Element in Sorted Array.c
@@ -1,6 +1,8 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+
+//method1
 int* searchRange(int* nums, int numsSize, int target, int* returnSize){
     
     
@@ -49,3 +51,92 @@ int* searchRange(int* nums, int numsSize, int target, int* returnSize){
     return store;
 
 }
+
+
+//method2 ver1
+int find_first(int *nums,int numsSize,int target)
+{
+    int left=0,right=numsSize-1,mid,firstpos=numsSize;   //用個變數firstpos來記可能的答案
+    
+    while(left<=right)
+    {
+        mid=left+(right-left)/2;
+        
+        if(nums[mid]>=target)
+        {
+            firstpos=mid;
+            right=mid-1;
+            
+        }
+        else
+            left=mid+1;
+    }
+    return firstpos;
+    
+}
+
+
+int* searchRange(int* nums, int numsSize, int target, int* returnSize){
+    
+    
+    int *store=(int *)malloc(sizeof(int)*2);
+    *returnSize=2;
+    
+    store[0]=find_first(nums,numsSize,target);
+    store[1]=find_first(nums,numsSize,target+1)-1;  //8的最後一個,可以想成>=9扣1
+    if(store[0]<=store[1])
+    {
+        return store;
+    }
+    else    //利用 store[1]有多扣1這個特性來防止都找不到時store[0]和store[1]會一樣,也可防止完全沒有,ex:[7 8 12] target:10
+    {
+        store[0]=-1;
+        store[1]=-1;
+    }
+    return store;
+    
+}
+
+
+//method2 ver2
+int find_first(int *nums,int numsSize,int target)
+{
+    int left=0,right=numsSize-1,mid,firstpos=numsSize;   //用個變數firstpos來記可能的答案
+    
+    while(left<=right)
+    {
+        mid=left+(right-left)/2;
+        
+        if(nums[mid]>=target)
+        {
+            firstpos=mid;
+            right=mid-1;
+            
+        }
+        else
+            left=mid+1;
+    }
+    return firstpos;
+    
+}
+
+
+int* searchRange(int* nums, int numsSize, int target, int* returnSize){
+    
+    
+    int *store=(int *)malloc(sizeof(int)*2);
+    *returnSize=2;
+    
+    store[0]=find_first(nums,numsSize,target);
+    if(store[0]==numsSize||nums[store[0]]!=target)  //firstpos exist and same as target
+    {
+        store[0]=-1;
+        store[1]=-1;
+        return store;
+    }
+    store[1]=find_first(nums,numsSize,target+1)-1;  //8的最後一個,可以想成>=9扣1  不用做檢查是因為store0(131行)已經檢查過了
+    return store;
+    
+}
+
+
