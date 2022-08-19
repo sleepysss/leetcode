22e820bf06bd0411bbe7eacbdbb58a97854e3785
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-08-20 02:00:37 +0800

    Update 2_Add Two Numbers.c

--- 2_Add Two Numbers.c
+++ 2_Add Two Numbers.c
@@ -1,3 +1,5 @@
+//method 1
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -33,3 +35,65 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
     }
     return head;
 }
+
+//method 2
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+    
+    //general:link to l1
+    //except 1.l1 first end ---> link to l2
+    //except 2.l2 first end ---> link to l1
+    // be careful of carry
+    
+    
+    int carry=0,sum,x,y;
+    struct ListNode dummy;
+    struct ListNode *ptr=&dummy, *carry_ptr;
+    
+    while(l1||l2||carry)
+    {
+        x=l1?l1->val:0;
+        y=l2?l2->val:0;
+        sum=x+y+carry;
+        
+        if(!l1&&!l2&&carry)
+        {
+            carry_ptr=(struct ListNode *)malloc(sizeof(struct ListNode));
+            carry_ptr->next=NULL;
+            ptr->next=carry_ptr;
+        }
+        else if(!l1)
+        {
+            ptr->next=l2;
+            l2=l2->next;
+
+        }
+        else if(!l2)
+        {
+            ptr->next=l1;
+            l1=l1->next;
+        }
+        else
+        {
+            ptr->next=l1;
+            l1=l1->next;
+            l2=l2->next;
+        }
+        ptr=ptr->next;
+        ptr->val=sum%10;
+        carry=sum/10;
+    }
+    return dummy.next;
+    
+}
+
+
