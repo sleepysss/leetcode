f7d95825b0c621769bd680f43edf2fb2fc82e5c4
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-09-03 00:23:14 +0800

    Update 33_Search in Rotated Sorted Array.c

--- 33_Search in Rotated Sorted Array.c
+++ 33_Search in Rotated Sorted Array.c
@@ -1,26 +1,35 @@
 int search(int* nums, int numsSize, int target){
-     
-    int left=0,right=numsSize-1,mid;
+    
+    int mid,left=0,right=numsSize-1;
     
     while(left<=right)
     {
         mid=left+(right-left)/2;
-        if(nums[mid]==target)
+        if(nums[mid]==target) //find
             return mid;
-        else if(nums[mid]>nums[right])  //mid at left sub
-        {
-            if(nums[mid]>target&&target>=nums[left])  //target at nums[left]~nums[mid-1]
-                right=mid-1;
-            else  //target at nums[mid+1]~nums[right]
-                left=mid+1;
+        if(nums[mid]>=nums[left]) //mid at left-sub(因為至少會大於等於最左邊)
+        {                         //等於發生在只有一個元素
+            if(nums[mid]>target&&target>=nums[left]) //target at mid's left
+            {
+                right=mid-1; //去mid左邊找
+            }
+            else
+            {
+                left=mid+1; //target不是上面那區,所以去另一區(mid右邊)找
+            }
         }
-        else  //mid at right sub
+        else
         {
-            if(nums[mid]<target&&target<=nums[right]) //target at nums[mid+1]~nums[right]
+            if(nums[right]>=target&&target>nums[mid]) //target at mid's right
+            {
                 left=mid+1;
-            else //target at nums[left]~nums[mid-1]
+            }
+            else
+            {
                 right=mid-1;
-        }    
+            }
+        }
     }
-    return -1;
+    
+    return -1; //找不到
 }
