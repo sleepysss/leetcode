1ae3a7f9e1ab782900f7369ec12df59e145a2e30
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-09-03 23:42:33 +0800

    Update 35_Search Insert Position.c

--- 35_Search Insert Position.c
+++ 35_Search Insert Position.c
@@ -1,15 +1,14 @@
 int searchInsert(int* nums, int numsSize, int target){
-    int left=0,right=numsSize,mid;
     
-    while(left<right)
+    int left=0,right=numsSize-1,mid;
+    
+    while(left<=right)
     {
         mid=left+(right-left)/2;
-        if(nums[mid]==target)
-            return mid;
-        if(nums[mid]>target)
-            right=mid;
-        else
+        if(nums[mid]<target)
             left=mid+1;
+        else
+            right=mid-1;
     }
     return left;
 }
