e25399457a4228b616b32ac64355e50e24502392
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-09-04 00:41:41 +0800

    Create 374_ Guess Number Higher or Lower.c

---
+++ 374_ Guess Number Higher or Lower.c
@@ -0,0 +1,25 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+int guessNumber(int n){
+    
+    int left=1,right=n,mid,store;
+    
+    for(;;)
+    {
+        mid=left+(right-left)/2;
+        store=guess(mid);
+        if(store==-1)
+            right=mid-1;
+        else if(store==1)
+            left=mid+1;
+        else
+            return mid;
+    }
+}
