5f7c200a77c420f6a3705f72a8fe9c1a5bee9a03
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-09-05 16:09:31 +0800

    Update 69_Sqrt(x).c

--- 69_Sqrt(x).c
+++ 69_Sqrt(x).c
@@ -1,53 +1,16 @@
-//brute force
 int mySqrt(int x){
     
-    long int store=0;
+    long left=1,right=x,mid;
     
-    for(long int i=0;i<=x;++i)
-    {
-        if(i*i<=x)
-            store=i;
-        else
-            break;
-    }
-    return store;
-    
-}
-
-//***binary search template***
-
-// range   [l,r) left close right open
-// function binarysearch(l,r)
-// {
-//   while(l<r)
-//   {
-//     m=l+(r-l)/2 same as m=(l+r)/2
-//     if f(m) return m (optional)
-//     if g(m) 
-//       r=m
-//     else
-//       l=m+1
-//   }
-//   return l or not found(if f() exist)
-//   l will equal to r after while(because loop exist if l<r),and  will be smallest index that g() be true after execute binary search  (given a function g(mid),returns the smallest mid in the range such that g(mid) holds true)
-// }
-
-//binary search
-
-int mySqrt(int x){
-    long long int left=1;
-    long long right=(long long)x+1;
-    long long mid;
-    while(left<right)
+    while(left<=right)
     {
         mid=left+(right-left)/2;
-        
         if(mid*mid>x)
-            right=mid;
+            right=mid-1;
         else
             left=mid+1;
     }
-    return left-1;
+    return right;  //或left-1都行
 }
 
 
