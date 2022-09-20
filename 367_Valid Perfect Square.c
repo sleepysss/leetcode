7a61f22c0a11b401b044aed79c2629052e3c1157
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-09-21 00:52:02 +0800

    Create 367_Valid Perfect Square.c

---
+++ 367_Valid Perfect Square.c
@@ -0,0 +1,16 @@
+bool isPerfectSquare(int num){
+    
+    long left=1,right=num,mid;
+    
+    while(left<=right)
+    {
+        mid=left+(right-left)/2;
+        if(mid*mid==num)
+            return true;
+        else if(mid*mid>num)
+            right=mid-1;
+        else
+            left=mid+1;
+    }
+    return false;
+}
