b697420dad5d42935db1487ca74a695ea19cd004
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-09-22 19:01:06 +0800

    Update 495_Teemo Attacking.c

--- 495_Teemo Attacking.c
+++ 495_Teemo Attacking.c
@@ -1,6 +1,8 @@
 int findPoisonedDuration(int* timeSeries, int timeSeriesSize, int duration){
     
-    int okay_time=-1,count=0;
+    //T毛可愛:)
+    
+    int okay_time=-1,count=0; //okay_time:正常狀態的時間 okay_time為2代表地2秒(含)以後為正常的
     
     for(int i=0;i<timeSeriesSize;++i)
     {
