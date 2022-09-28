4e2e05dfd75256ca77bcc0fdc848a766d2dcea19
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-09-28 12:08:59 +0800

    Update 328_Odd Even Linked List.c

--- 328_Odd Even Linked List.c
+++ 328_Odd Even Linked List.c
@@ -8,23 +8,23 @@
 
 struct ListNode* oddEvenList(struct ListNode* head){
     
-    if(!head||!head->next)
+    //啥都沒
+    if(!head)
         return head;
     
     struct ListNode *odd=head, *even=head->next, *even_head=head->next;
     
-    //偶數個的情況到最後面odd為NULL,even為NULL
-    //奇數個的情況到最後面odd非NULL,even為NULL
+    //偶數個node的情況到最後面會是odd為NULL,even為NULL
+    //奇數個node的情況到最後面會是odd非NULL,even為NULL
     //所以條件設為even&&even->next
     while(even&&even->next)
     {
-        odd->next=even->next; //改成指向原本下個節點的下一個節點
-        even->next=even->next->next; //改成指向原本下個節點的下一個節點
+        odd->next=even->next; //改成指向原本下個節點的下一個節點 1->3
+        even->next=even->next->next; //改成指向原本下個節點的下一個節點 2->4
         odd=odd->next;  //next round
         even=even->next;
     }
     odd->next=even_head; //link
     
     return head;
-    
 }
