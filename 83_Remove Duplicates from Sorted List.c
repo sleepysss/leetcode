7dea68a3fbbdc9ea184a060836598cf342364227
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-09-28 23:34:51 +0800

    Update 83_Remove Duplicates from Sorted List.c

--- 83_Remove Duplicates from Sorted List.c
+++ 83_Remove Duplicates from Sorted List.c
@@ -2,19 +2,17 @@ struct ListNode* deleteDuplicates(struct ListNode* head){
     
     if(!head)
         return NULL;
-    
-    int num=head->val;
+
     struct ListNode *ptr=head->next, *pre=head;
     
     while(ptr)
     {
-        if(ptr->val==num)
+        if(ptr->val==pre->val)
         {
             pre->next=ptr->next;
             free(ptr);
             ptr=pre;
         }
-        num=ptr->val;
         pre=ptr;
         ptr=ptr->next;
     }
