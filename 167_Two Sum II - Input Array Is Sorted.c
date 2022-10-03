5e61ac6c6ead929283f15c1e7b66810563f43702
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-10-04 00:54:05 +0800

    Update 167_Two Sum II - Input Array Is Sorted.c

--- 167_Two Sum II - Input Array Is Sorted.c
+++ 167_Two Sum II - Input Array Is Sorted.c
@@ -1,6 +1,9 @@
+//method 1 :  two pointer
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+
 int* twoSum(int* numbers, int numbersSize, int target, int* returnSize){
     
     int *store=(int *)malloc(sizeof(int)*2);
@@ -18,6 +21,48 @@ int* twoSum(int* numbers, int numbersSize, int target, int* returnSize){
             l++;
         else
             r--;
+    }
+    *returnSize=2;
+    return store;
+}
+
+//method 2 : binary search
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+int* twoSum(int* numbers, int numbersSize, int target, int* returnSize){
+    
+    //想法:列舉每個索引i作為index1，對其右方的數字做二分搜，找出target扣掉numbers[i]後還需要的值n
+    
+    int left,right,mid,num=target,flag=0;
+    int *store=(int *)malloc(sizeof(int)*2);
+    
+    for(int i=0;i<numbersSize;++i)
+    {
+        //binary search
+        num=target-numbers[i]; //target of binary search
+        left=i+1;
+        right=numbersSize-1;
+        
+        while(left<=right)
+        {
+            mid=left+(right-left)/2;
+            if(numbers[mid]==num)
+            {
+                store[0]=i+1;
+                store[1]=mid+1;
+                flag=1;
+                break;
+            }
+            else if(numbers[mid]>num)
+                right=mid-1;
+            else
+                left=mid+1;
+        }
+        if(flag)
+            break; 
     }
     *returnSize=2;
     return store;
