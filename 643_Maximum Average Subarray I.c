e757c4245b9273ddc8155068d8f8c9f6553eae13
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-10-16 14:48:10 +0800

    Update 643_Maximum Average Subarray I.c

--- 643_Maximum Average Subarray I.c
+++ 643_Maximum Average Subarray I.c
@@ -23,3 +23,30 @@ double findMaxAverage(int* nums, int numsSize, int k){
     
     return max_avg;
 }
+
+//while 改for
+double findMaxAverage(int* nums, int numsSize, int k){
+    
+    int start=0,end=k-1,sum=0;
+    double max_avg,avg;
+    
+    //init
+    for(int i=0;i<k;++i)
+    {
+        sum+=nums[i];
+    }
+    max_avg=(double)sum/(double)k;
+    
+    for(int i=k;i<numsSize;++i) //i:目前window的下一個
+    {
+        //new window
+        sum-=nums[i-k];
+        sum+=nums[i];
+        
+        avg=(double)sum/(double)k;
+        if(avg>max_avg)
+            max_avg=avg;
+    }
+    
+    return max_avg;
+}
