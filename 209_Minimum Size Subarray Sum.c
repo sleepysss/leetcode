2b0b3adc973aefa342ef29fad47fc3961de959af
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-10-16 11:24:56 +0800

    Update 209_Minimum Size Subarray Sum.c

--- 209_Minimum Size Subarray Sum.c
+++ 209_Minimum Size Subarray Sum.c
@@ -1,13 +1,14 @@
+//method 1 : brute force ... TLE
 int minSubArrayLen(int target, int* nums, int numsSize){
     
     int count,total,min_count=numsSize+1;
-    for(int i=0;i<numsSize;++i)
+    for(int i=0;i<numsSize;++i) //traverse every num
     {
         count=0;
         total=0;
-        for(int j=i;j<numsSize;++j)
+        for(int j=i;j<numsSize;++j) //candiadate of subarr
         {
-            if(total<target)
+            if(total<target) //add
             {
                 total+=nums[j];
                 count++;
@@ -16,12 +17,12 @@ int minSubArrayLen(int target, int* nums, int numsSize){
                 break;
         }
         
-        if(total>=target)
+        if(total>=target) //we need this because total may smaller than target 
         {
-            if(count<min_count)
+            if(count<min_count) //smaller than smallest
                 min_count=count;
         }
-        else
+        else //有num[i]都小於目標了,後面沒num[i]更不可能>=目標(因為<目標會一直加直到數列尾)
             break; 
     }
     
