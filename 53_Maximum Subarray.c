4225b3fdafefb018eb37a2992c5ccfd2854b8713
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-10-21 22:11:57 +0800

    Update 53_Maximum Subarray.c

--- 53_Maximum Subarray.c
+++ 53_Maximum Subarray.c
@@ -21,13 +21,13 @@ int maxSubArray(int* nums, int numsSize){
 //method 2 : 隊友強不強(?)理論
 int maxSubArray(int* nums, int numsSize){
     
-    int curr=0,max=-10001; //curr:curr's maxSubArray
+    int curr=0,max=-10001; //curr:curr's maxSubArray'
     
     //對於每個點來說,看看前人加上他後的值會不會比自己從新當頭來的好
     //ex: [-2,1]
     //對於1來說 -2+1 < 1 ,那我就從我自己開始新的探險,即:curr改為1 (不要帶拖油瓶)
     //curr=curr+nums[i]>nums[i] 等同 curr>0
-    //至於每次都要更新max的原因為可能遇到躺分仔 ex:[2,1,-5] ,若最後才更新會剩0而已
+    //至於每次都要更新max的原因為可能遇到躺分仔 ex:[2,1,-5] ,若最後才更新會剩-2而已(先組一隊,看未來的隊友強不強 ex:[2,3,-2,9999999])
     
     for(int i=0;i<numsSize;++i)
     {
