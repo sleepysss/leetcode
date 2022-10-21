33e4da47baadf98c7f9985fc469cd93a04a7b5a9
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-10-22 00:40:41 +0800

    Update 121_Best Time to Buy and Sell Stock.c

--- 121_Best Time to Buy and Sell Stock.c
+++ 121_Best Time to Buy and Sell Stock.c
@@ -17,7 +17,7 @@ int maxProfit(int* prices, int pricesSize){
 int maxProfit(int* prices, int pricesSize){
     
     //核心想法:可能解 = 目前價錢 - 前面出現的最低價格
-    //store[i] 0到i天的最低價格
+    //store[i] 0到i天的最低價格 
     int *store=(int *)malloc(sizeof(int)*pricesSize);
     
     store[0]=prices[0];
@@ -27,6 +27,20 @@ int maxProfit(int* prices, int pricesSize){
     {
         max=max>(prices[i]-store[i-1])?max:prices[i]-store[i-1];
         store[i]=store[i-1]>prices[i]?prices[i]:store[i-1];
+    }
+    return max;
+}
+
+//method 3 : 和DP概念一樣,只是改成用一變數存到目前的最小價格,而不存對每一天來說的最小價格
+
+int maxProfit(int* prices, int pricesSize){
+    
+    int max=0,min_price=prices[0];
+    
+    for(int i=1;i<pricesSize;++i)
+    {
+        max=max>(prices[i]-min_price)?max:prices[i]-min_price;
+        min_price=min_price>prices[i]?prices[i]:min_price;
     }
     return max;
 }
