4ba3c492f08d8b3993fa628984725886dd33c626
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-10-21 23:17:50 +0800

    Update 121_Best Time to Buy and Sell Stock.c

--- 121_Best Time to Buy and Sell Stock.c
+++ 121_Best Time to Buy and Sell Stock.c
@@ -1,3 +1,4 @@
+//method 1 : brute force...TLE
 int maxProfit(int* prices, int pricesSize){
     
     int max=0;
@@ -8,6 +9,23 @@ int maxProfit(int* prices, int pricesSize){
         {
             max=max>(prices[j]-prices[i])?max:prices[j]-prices[i];
         }
+    }
+    return max;
+}
+
+//method 2 : DP
+int maxProfit(int* prices, int pricesSize){
+    
+    //store[i] 0到i的最低價格
+    int *store=(int *)malloc(sizeof(int)*pricesSize);
+    
+    store[0]=prices[0];
+    int max=0;
+    
+    for(int i=1;i<pricesSize;++i)
+    {
+        max=max>(prices[i]-store[i-1])?max:prices[i]-store[i-1];
+        store[i]=store[i-1]>prices[i]?prices[i]:store[i-1];
     }
     return max;
 }
