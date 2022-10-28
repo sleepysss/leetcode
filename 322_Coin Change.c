614dda6c2c46f9486343bece5305d83f42e2e9e8
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-10-28 19:39:34 +0800

    Update 322_Coin Change.c

--- 322_Coin Change.c
+++ 322_Coin Change.c
@@ -4,7 +4,7 @@ int coinChange(int* coins, int coinsSize, int amount){
     
     // n 為金額， c1… ck為面額
     //f(n) = Min( 1 + f(n- c1), 1+ f(n-c2),…, 1+f(n-ck) )
-    //有點想爬樓梯那題的概念
+    //有點像爬樓梯那題的概念
     
     
     int needcoin,min=10001;
