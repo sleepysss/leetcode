71d8addbd5f0fdc1737971aa3c758d3eed80bb6d
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-11-01 16:18:58 +0800

    Update 46_Permutation.c

--- 46_Permutation.c
+++ 46_Permutation.c
@@ -1,3 +1,5 @@
+//method 1-a
+
 /**
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
@@ -52,3 +54,76 @@ int** permute(int* nums, int numsSize, int* returnSize, int** returnColumnSizes)
     per(nums,visit,-1,&count,numsSize,temp);
     return store;
 }
+
+
+
+//method 1-b
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+
+int count_num(int num) //Factorial
+{
+    int store=1;
+    for(int i=1;i<=num;++i)
+        store*=i;
+    return store;
+}
+
+void permutation_helper(int *helper,int count,int *visited,int **store,int *where,int *numsSize,int *nums)  //where 放到store的哪了,count:第幾層
+{
+    if(count==numsSize)
+    {
+        //把helper內的東西填入store中
+        for(int i=0;i<numsSize;++i)
+        {
+            store[(*where)][i]=helper[i];
+        }
+        (*where)++;
+        return;
+    }
+    count++; //next level
+    for(int i=0;i<numsSize;++i)
+    {
+        if(!visited[i])
+        {
+            //做選擇
+            helper[count-1]=nums[i]; //加入候選解
+            visited[i]=1;
+            
+            permutation_helper(helper,count,visited,store,where,numsSize,nums);
+            
+            //撤銷選擇
+            visited[i]=0;
+            //helper[count-1]=0;
+        }
+    }
+}
+
+int** permute(int* nums, int numsSize, int* returnSize, int** returnColumnSizes){
+    
+    int num=count_num(numsSize),where=0;
+    int *helper=(int *)malloc(sizeof(int)*numsSize); //存路徑
+    int *visited=(int *)malloc(sizeof(int)*numsSize); //visit or not
+    for(int i=0;i<numsSize;++i)
+        visited[i]=0;
+    
+    int **store=(int **)malloc(sizeof(int *)*num);
+    for(int i=0;i<num;++i)
+        store[i]=(int *)malloc(sizeof(int)*numsSize);
+    
+    permutation_helper(helper,0,visited,store,&where,numsSize,nums);
+    
+    int *ptr=(int *)malloc(sizeof(int)*num);
+    for(int i=0;i<num;++i)
+        ptr[i]=numsSize;
+    *returnColumnSizes=ptr;
+    *returnSize=num;
+    
+    return store; 
+}
+
+
