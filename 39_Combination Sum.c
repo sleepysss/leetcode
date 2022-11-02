21a7376e82c7bd1f5a7e633adb137e68f2ca80fe
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-11-02 18:15:04 +0800

    Update 39_Combination Sum.c

--- 39_Combination Sum.c
+++ 39_Combination Sum.c
@@ -58,3 +58,65 @@ int** combinationSum(int* candidates, int candidatesSize, int target, int* retur
     *returnColumnSizes=return_arr2;
     return store1;
 }
+
+
+
+
+
+//和上面寫法一樣,只是helper_total和helper_index改成call by value
+//helper_total和helper_index可以不用pass by addr,但where一定要
+
+void combinationSum_helper(int *candidates,int candidatesSize,int target,int helper_total,int helper_index,int **store,int *where_addr,int *return_arr,int *helper,int start) //start:可選的開始
+{
+    if(helper_total==target)
+    {
+        return_arr[(*where_addr)]=helper_index;
+        store[(*where_addr)]=(int *)malloc(sizeof(int)*helper_index);
+        for(int i=0;i<helper_index;++i)
+            store[(*where_addr)][i]=helper[i];
+        (*where_addr)++;
+        return;
+    }
+    else if(helper_total>target)
+    {
+        return;
+    }
+    
+    for(int i=start;i<candidatesSize;++i) //[2 2 3] 和 [3 2 2] 視為同一個,所以要有start這個東西
+    {
+        helper[helper_index++]=candidates[i];
+        helper_total+=candidates[i];
+        
+        combinationSum_helper(candidates,candidatesSize,target,helper_total,helper_index,store,where_addr,return_arr,helper,i);
+        
+        helper_index--;
+        helper_total-=candidates[i];
+    }
+}
+
+int** combinationSum(int* candidates, int candidatesSize, int target, int* returnSize, int** returnColumnSizes){
+    
+    int **store=(int **)malloc(sizeof(int *)*150), **store1;  //store[]最多只會有150列(題目給的)
+    for(int i=0;i<150;++i) //初始化,好方便之後算實際究竟有幾個
+        store[i]=NULL;
+    int where=0,helper_index=0,index=0,helper_total=0;
+    int *helper=(int *)malloc(sizeof(int)*150), *return_arr2; //每列最多20個數字 (40/2=20)
+    int *return_arr=(int *)malloc(sizeof(int)*150);
+    
+    
+    combinationSum_helper(candidates,candidatesSize,target,helper_total,helper_index,store,&where,return_arr,helper,0);
+    
+    //算實際數量
+    for(int i=0;i<150;++i)
+    {
+        if(store[i]==NULL)
+        {
+            *returnSize=i;
+            return_arr2=realloc(return_arr,sizeof(int)*i);
+            store1=realloc(store,sizeof(int *)*i);
+            break;
+        }  
+    }
+    *returnColumnSizes=return_arr2;
+    return store1;
+}
