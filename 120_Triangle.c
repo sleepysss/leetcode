440018962fd38a55f475fff2d56d6da247a501b2
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-11-04 16:39:33 +0800

    Update 120_Triangle.c

--- 120_Triangle.c
+++ 120_Triangle.c
@@ -1,4 +1,5 @@
-//naive recursive
+//naive recursive ... TLE
+
 int min(int a,int b)
 {
     return a>b?b:a;
@@ -14,4 +15,31 @@ int minimumTotal_helper(int **triangle,int triangleSize,int start_col_index)
 
 int minimumTotal(int** triangle, int triangleSize, int* triangleColSize){
     return minimumTotal_helper(triangle,triangleSize,0);
+}
+
+
+//method 2 : DP
+
+int min(int a,int b)
+{
+    return a>b?b:a;
+}
+
+int minimumTotal(int** triangle, int triangleSize, int* triangleColSize){
+
+    int store[triangleSize][triangleColSize[triangleSize-1]],back; //store[i][j]=minmumTotal(triangle's head=[i,j])
+
+    //init
+    for(int i=0;i<triangleColSize[triangleSize-1];++i)
+        store[triangleSize-1][i]=triangle[triangleSize-1][i];  //最下面一列先設為各自的數字
+
+    for(int i=triangleSize-2;i>=0;--i)
+    {
+        for(int j=0;j<=i;++j)
+        {
+            store[i][j]=triangle[i][j]+min(store[i+1][j],store[i+1][j+1]);
+        }
+    }
+
+    return store[0][0];
 }
