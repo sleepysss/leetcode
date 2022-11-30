f6c7da36e90e69b5364dccd7ccf06c0365c3c7d8
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-11-30 12:05:47 +0800

    Update 977_Squares of a Sorted Array.cpp

--- 977_Squares of a Sorted Array.cpp
+++ 977_Squares of a Sorted Array.cpp
@@ -12,3 +12,57 @@ public:
         return nums;
     }
 };
+
+//method 2-1
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+
+        int left=0,right=nums.size()-1; 
+        vector<int> store(nums);
+
+        for(int i=store.size()-1;i>=0;--i)
+        {
+            if(abs(nums[left])>abs(nums[right]))
+            {
+                store[i]=nums[left]*nums[left];
+                left++;
+            }
+            else
+            {
+                store[i]=nums[right]*nums[right];
+                right--;
+            }
+        }
+
+        return store;
+    }
+};
+
+//method 2-2
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+
+        int left=0,right=nums.size()-1,pos=nums.size()-1; 
+        vector<int> store(nums);
+
+        while(left<=right)
+        {
+            if(abs(nums[left])>abs(nums[right]))
+            {
+                store[pos--]=nums[left]*nums[left];
+                left++;
+            }
+            else
+            {
+                store[pos--]=nums[right]*nums[right];
+                right--;
+            }
+        }
+
+        return store;
+    }
+};
