fe87cb6d0055c3438ae2b9ffc268351d8d0e90e9
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-11-30 13:02:38 +0800

    Create 15_3Sum.cpp

---
+++ 15_3Sum.cpp
@@ -0,0 +1,44 @@
+// method 1 : brute force ... TLE
+
+class Solution {
+public:
+
+    bool duplicate(const vector<vector<int>> &ans,const vector<int> &tmp)
+    {
+        for(int i=0;i<ans.size();++i)
+        {
+            if(ans[i][0]==tmp[0]&&ans[i][1]==tmp[1]&&ans[i][2]==tmp[2])
+                return true;
+        }
+        return false;
+    }
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<nums.size();++i)
+        {
+            if(i&&nums[i]==nums[i-1])
+                continue;
+            else
+            {
+                for(int j=i+1;j<nums.size();++j)
+                {
+                    for(int k=j+1;k<nums.size();++k)
+                    {
+                        if(nums[i]+nums[j]+nums[k]==0)
+                        {
+                            //check duplicate
+                            vector<int> tmp={nums[i],nums[j],nums[k]};
+                            if(!duplicate(ans,tmp))
+                                ans.push_back(tmp);   
+                        }
+                    }
+                }
+            }
+            
+        }
+        return ans;
+    }
+};
