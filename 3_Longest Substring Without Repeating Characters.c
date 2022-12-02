23187eff20247835606c1ada98a8dee739a93f49
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-12-02 14:03:27 +0800

    Update 3_Longest Substring Without Repeating Characters.c

--- 3_Longest Substring Without Repeating Characters.c
+++ 3_Longest Substring Without Repeating Characters.c
@@ -1,3 +1,5 @@
+//method 1 
+
 //想法:只要當前判斷的字元不存在於建立的陣列中，即將該字元加入此陣列；如果這個字元已經存在陣列中，則一直刪除陣列開頭的值，直到當前判斷的字元不存在於陣列中為止。
 //即:維護一個滑動窗口(start~i)裡面不能有重複的元素
 
@@ -46,7 +48,7 @@ int lengthOfLongestSubstring(char * s){
 
 
 
-//brute force:對每一個s中的char都去嘗試(以他當開頭(的字串)),直到碰到一樣的,並且去比較長度
+// method 2 : brute force:對每一個s中的char都去嘗試(以他當開頭(的字串)),直到碰到一樣的,並且去比較長度
 
 int lengthOfLongestSubstring(char * s){
     int store[128]={0},i=0,j,ans=0,count;  //ascii 
@@ -97,3 +99,30 @@ int lengthOfLongestSubstring(char * s){
     }
     return max_count;
 }
+
+//method 3 
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        int ascii[128],max=0,right=0,left=0; //ascii:each character first appear's index (-1:does not appear yet)  (定義和method1的不一樣)
+
+        for(int i=0;i<128;++i)
+            ascii[i]=-1;
+
+        for(right=0;s[right];++right)
+        {
+            if(ascii[s[right]]>=left&&ascii[s[right]]!=-1)  //char is duplicate inside window
+            {
+                left=ascii[s[right]]+1;
+                ascii[s[right]]=right;
+            }
+            else
+                ascii[s[right]]=right;
+
+            max=max>(right-left+1)?max:right-left+1;
+        }
+        return max;
+    }
+};
