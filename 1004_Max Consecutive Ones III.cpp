faf2380404d4a6a50b47b91f402fcf87dd95e519
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-12-02 13:01:51 +0800

    Create 1004_Max Consecutive Ones III.cpp

---
+++ 1004_Max Consecutive Ones III.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+
+        int left=0,right=0,count0=0,maxsize=0,size=0;
+
+        for(right=0;right<nums.size();++right)
+        {
+            if(!nums[right])
+                count0++;
+               
+            while(count0>k)
+            {
+                size--;
+                if(!nums[left++])
+                    count0--;
+            }
+            size=right-left+1; //cal the length of valid subarr
+            maxsize=maxsize>size?maxsize:size;
+        }
+        return maxsize;
+    }
+};
