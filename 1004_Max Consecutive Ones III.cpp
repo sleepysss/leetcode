a5611cf155eb3da9be582e73040d08b898a62f8b
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-12-02 13:04:42 +0800

    Update 1004_Max Consecutive Ones III.cpp

--- 1004_Max Consecutive Ones III.cpp
+++ 1004_Max Consecutive Ones III.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
 
-        int left=0,right=0,count0=0,maxsize=0,size=0;
+        int left=0,right=0,count0=0,maxsize=0,size=0; //count0:將0變為1的個數
 
         for(right=0;right<nums.size();++right)
         {
