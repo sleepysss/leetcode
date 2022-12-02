9cce8e72670e5c48ef016c5ecbec8d6501ba3766
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-12-02 13:16:21 +0800

    Update 485_Max Consecutive Ones.cpp

--- 485_Max Consecutive Ones.cpp
+++ 485_Max Consecutive Ones.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-
+        //掃一遍，遇到 0 就把 counter 歸零，找最大
         int ans=0,count=0;
 
         for(int i=0;i<nums.size();++i)
