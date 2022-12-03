4b4951c79e72a84b41d7044a01a7c7d1992fb68f
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-12-03 16:35:09 +0800

    Update 217_Contains Duplicate.cpp

--- 217_Contains Duplicate.cpp
+++ 217_Contains Duplicate.cpp
@@ -1,3 +1,5 @@
+//method 1 : hash table
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
@@ -9,6 +11,25 @@ public:
             if(m.count(nums[i]))
                 return true;
             m[nums[i]]=i;
+        }
+        return false;
+    }
+};
+
+
+//method 2 : sorting
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        //先將 array 中的數字按大小排序，這樣只需要用一個loop比較自己跟自己身邊的數字就可以，不需要比較鄰居以外的地方
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<nums.size()-1;++i)
+        {
+            if(nums[i]==nums[i+1])
+                return true;
         }
         return false;
     }
