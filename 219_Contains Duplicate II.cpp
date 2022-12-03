403e1f05a63e0968b88edbc6056add49ea12315a
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-12-03 16:05:00 +0800

    Update 219_Contains Duplicate II.cpp

--- 219_Contains Duplicate II.cpp
+++ 219_Contains Duplicate II.cpp
@@ -15,3 +15,22 @@ public:
         return false;   
     }
 };
+
+//method 2 : hash table
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+
+        unordered_map<int,int> m;
+
+        for(int i=0;i<nums.size();++i)
+        {
+            if((m.count(nums[i]))&&(i-m[nums[i]]<=k)) //用hash table來檢查是否已在裡面(效率為O(1),比慢慢一個一個比O(n)好)
+                return true;
+
+            m[nums[i]]=i;
+        }
+        return false;
+    }
+};
