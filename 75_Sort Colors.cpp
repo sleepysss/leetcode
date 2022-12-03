5411b5c610cc7caaf62b0795c0b864ea5e6a115e
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-12-03 20:34:53 +0800

    Update 75_Sort Colors.cpp

--- 75_Sort Colors.cpp
+++ 75_Sort Colors.cpp
@@ -17,3 +17,33 @@ public:
 
     }
 };
+
+
+//method 2 : two pointer
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        //用類似quicksort lomuto那個方法
+        int red=0,blue=nums.size()-1; //red左的index皆為red,blue右的index皆為blue
+        for(int i=0;i<=blue;++i) //blue右的index皆為blue,所以<=blue就好
+        {
+            if(nums[i]==0)
+            {
+                int tmp=nums[i];
+                nums[i]=nums[red];
+                nums[red]=tmp;
+                red++;
+                //因為是從左到右traverse,2必定已被換掉,所以不用i--
+            }
+            else if(nums[i]==2)
+            {
+                int tmp=nums[i];
+                nums[i]=nums[blue];
+                nums[blue]=tmp;
+                blue--;
+                i--;  //因為可能會把0 swap到原本位置
+            }
+        }
+    }
+};
