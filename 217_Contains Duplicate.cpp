adfc3f4ea2e37445b9a785a66455466508090904
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-12-03 16:23:21 +0800

    Create 217_Contains Duplicate.cpp

---
+++ 217_Contains Duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        unordered_map<int,int> m;
+
+        for(int i=0;i<nums.size();++i)
+        {
+            if(m.count(nums[i]))
+                return true;
+            m[nums[i]]=i;
+        }
+        return false;
+    }
+};
