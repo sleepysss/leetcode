0de6e31b63747bd1975d37d3e355ecf5ddec6fe8
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-12-12 21:38:26 +0800

    Update 198_House Robber.cpp

--- 198_House Robber.cpp
+++ 198_House Robber.cpp
@@ -27,6 +27,8 @@ public:
 
 //method 2 : naive recursive
 
+//max = Max( 現在這棟 + 前前一棟可以拿到的最大金額 , 前一棟可以拿到的最大金額 )
+
 class Solution {
 public:
 
@@ -51,6 +53,11 @@ public:
 class Solution {
 public:
 
+    int max(int a,int b)
+    {
+        return a>b?a:b;
+    }
+
     int rob(vector<int>& nums) {
 
         if(nums.size()==1)
@@ -59,10 +66,10 @@ public:
         vector<int> store(nums.size());   //store[i]:index 0~i能搶到的最多錢
 
         store[0]=nums[0];
-        store[1]=nums[0]>nums[1]?nums[0]:nums[1];
+        store[1]=max(nums[0],nums[1]);
 
         for(int i=2;i<nums.size();++i)
-            store[i]=(store[i-1]>(store[i-2]+nums[i]))?store[i-1]:store[i-2]+nums[i]; //i不搶(store[i-1])或i搶+store[i-2]
+            store[i]=max(store[i-1],store[i-2]+nums[i]); //i不搶(store[i-1])或i搶+store[i-2]
 
         return store[nums.size()-1];
     }
