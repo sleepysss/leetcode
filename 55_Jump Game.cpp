7fc695b71c92f98c51b671056a24c10a293e669b
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-12-14 19:15:06 +0800

    Update 55_Jump Game.cpp

--- 55_Jump Game.cpp
+++ 55_Jump Game.cpp
@@ -1,4 +1,4 @@
-//method 1 : naive recursive
+//method 1 : naive recursive  ...TLE
 
 public:
 
@@ -21,5 +21,26 @@ public:
     bool canJump(vector<int>& nums) {
 
         return helper(nums,0); 
+    }
+};
+
+//method 2 : DP ...TLE
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+
+        vector<int> store(nums.size()); //到每個點的可能性
+
+        store[0]=1;
+        //從 index 0，也就是起點開始，每一個位置都更新一輪 store[]
+        for(int i=0;i<nums.size()-1;++i)
+        {
+            if(!store[i]) //不會到那位置
+                continue;
+            for(int j=1;(j+i)<nums.size()&&j<=nums[i];++j)
+                store[i+j]=1;
+        }
+        return store[nums.size()-1];
     }
 };
