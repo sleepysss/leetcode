651c9a405ba3da4bb7f0b7c2916b177517c4dadc
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-12-15 20:16:06 +0800

    Update 152_Maximum Product Subarray.cpp

--- 152_Maximum Product Subarray.cpp
+++ 152_Maximum Product Subarray.cpp
@@ -1,3 +1,5 @@
+//method 1:
+
 class Solution {
 public:
 
@@ -24,5 +26,42 @@ public:
             ans=MAX(max,ans);
         }
         return ans;
+    }
+};
+
+//method 2 : method1 1改成DP
+
+class Solution {
+public:
+
+    int MAX(int a,int b)
+    {
+        return a>b?a:b;
+    }
+    int MIN(int a,int b)
+    {
+        return a<b?a:b;
+    }
+    int maxProduct(vector<int>& nums) {
+        
+        vector<int> store1(nums.size()); //store1[i]:max product subarr ends with index i
+        vector<int> store2(nums.size()); //store2[i]:min product subarr ends with index i
+        int max=nums[0];
+        store1[0]=store2[0]=nums[0];
+        for(int i=1;i<nums.size();++i)
+        {
+            if(nums[i]<0) //反過來
+            {
+                store1[i]=MAX(nums[i],nums[i]*store2[i-1]);
+                store2[i]=MIN(nums[i],nums[i]*store1[i-1]);
+            }
+            else
+            {
+                store1[i]=MAX(nums[i],nums[i]*store1[i-1]);
+                store2[i]=MIN(nums[i],nums[i]*store2[i-1]);
+            }   
+            max=max>store1[i]?max:store1[i];
+        }
+        return max;
     }
 };
