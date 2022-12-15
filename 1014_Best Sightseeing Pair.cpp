f99dd73c75ba9ee3a2269af81ecfd4632d0e2713
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-12-16 00:07:14 +0800

    Update 1014_Best Sightseeing Pair.cpp

--- 1014_Best Sightseeing Pair.cpp
+++ 1014_Best Sightseeing Pair.cpp
@@ -1,6 +1,10 @@
+//method 1
+
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
+        
+        //解法和best time to buy and sell stock 類似
 
         int max=values[0]+0,ans=0;  //max:记录之前出现过的 A[i] + i 的最大值
         for(int j=1;j<values.size();++j)
@@ -8,6 +12,24 @@ public:
             ans=max+values[j]-j>ans?max+values[j]-j:ans; //試試是否比已知ans更大
             max=values[j]+j>max?values[j]+j:max;
         }   
+        return ans;
+    }
+};
+
+//method 2 : method 1 改成DP版本
+
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& values) {
+
+        vector<int> store(values.size()); //store[i]: index 0~i中values[i]+i最大的
+        store[0]=values[0]+0;
+        int ans=0;
+        for(int j=1;j<store.size();++j)
+        {
+            ans=values[j]-j+store[j-1]>ans?values[j]-j+store[j-1]:ans;
+            store[j]=values[j]+j>store[j-1]?values[j]+j:store[j-1];
+        }
         return ans;
     }
 };
