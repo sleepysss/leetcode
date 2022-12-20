73cfa51d24e54b71dd72f4b3685cc0133b861379
sleepysss <86964370+sleepysss@users.noreply.github.com>
2022-12-20 15:23:35 +0800

    Update 88_Merge Sorted Array.c

--- 88_Merge Sorted Array.c
+++ 88_Merge Sorted Array.c
@@ -59,3 +59,20 @@ void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
     free(store);
 }
 
+//method 3 : 從後面回來(找較大的)就不用令一個array了
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        int i=m-1,j=n-1,count=m+n-1;
+
+        while(i>=0&&j>=0)
+            nums1[count--]=nums1[i]>nums2[j]?nums1[i--]:nums2[j--];
+        while(i>=0)
+            nums1[count--]=nums1[i--];
+        while(j>=0)
+            nums1[count--]=nums2[j--];
+    }
+};
+
