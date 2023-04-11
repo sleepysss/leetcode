9c23bde1b35d7e179bb9d1db1627fb6671e7d36e
sleepysss <86964370+sleepysss@users.noreply.github.com>
2023-04-11 14:52:01 +0800

    Create 622_Design Circular Queue.c

---
+++ 622_Design Circular Queue.c
@@ -0,0 +1,79 @@
+typedef struct {
+    int *store;
+    int front,rear,size;
+} MyCircularQueue;
+
+
+MyCircularQueue* myCircularQueueCreate(int k) {
+    MyCircularQueue *q=(MyCircularQueue *)malloc(sizeof(MyCircularQueue));
+    q->store=(int *)malloc(sizeof(int)*(k+1));
+    q->size=k+1;
+    q->front=q->rear=0;
+    return q;
+}
+
+bool myCircularQueueIsEmpty(MyCircularQueue* obj) {
+    if(obj&&(obj->front==obj->rear))
+        return true;
+    else
+        return false;
+}
+
+bool myCircularQueueIsFull(MyCircularQueue* obj) {
+    if(obj&&((obj->rear+1)%obj->size==obj->front))
+        return true;
+    else
+        return false;
+}
+
+bool myCircularQueueEnQueue(MyCircularQueue* obj, int value) {
+
+    if(myCircularQueueIsFull(obj))
+        return false;
+    obj->rear=(obj->rear+1)%obj->size;
+    obj->store[obj->rear]=value;
+    return true;
+}
+
+bool myCircularQueueDeQueue(MyCircularQueue* obj) {
+    if(myCircularQueueIsEmpty(obj))
+        return false;
+    obj->front=(obj->front+1)%obj->size;
+    return true;
+}
+
+int myCircularQueueFront(MyCircularQueue* obj) {
+    if(myCircularQueueIsEmpty(obj))
+        return -1;
+
+    return obj->store[(obj->front+1)%obj->size];
+}
+
+int myCircularQueueRear(MyCircularQueue* obj) {
+    if(myCircularQueueIsEmpty(obj))
+        return -1;
+    return obj->store[obj->rear];
+}
+
+void myCircularQueueFree(MyCircularQueue* obj) {
+    free(obj->store);
+    free(obj);
+}
+
+/**
+ * Your MyCircularQueue struct will be instantiated and called as such:
+ * MyCircularQueue* obj = myCircularQueueCreate(k);
+ * bool param_1 = myCircularQueueEnQueue(obj, value);
+ 
+ * bool param_2 = myCircularQueueDeQueue(obj);
+ 
+ * int param_3 = myCircularQueueFront(obj);
+ 
+ * int param_4 = myCircularQueueRear(obj);
+ 
+ * bool param_5 = myCircularQueueIsEmpty(obj);
+ 
+ * bool param_6 = myCircularQueueIsFull(obj);
+ 
+ * myCircularQueueFree(obj);
+*/
