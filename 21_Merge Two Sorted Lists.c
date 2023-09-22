5875d91a3894d429f0e80a88573acd94ceaff870
sleepysss <86964370+sleepysss@users.noreply.github.com>
2023-09-22 15:37:52 +0800

    Update 21_Merge Two Sorted Lists.c

--- 21_Merge Two Sorted Lists.c
+++ 21_Merge Two Sorted Lists.c
@@ -90,6 +90,47 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
 
 
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+        ListNode dummy;
+        dummy.next=NULL;
+        ListNode *ptr=&dummy;
+        while(list1&&list2)
+        {
+            if(list1->val<list2->val)
+            {
+                ptr->next=list1;
+                list1=list1->next;
+            }
+            else
+            {
+                ptr->next=list2;
+                list2=list2->next;
+            }
+            ptr=ptr->next;
+        }
+        
+        ptr->next = list1?list1:list2;
+        
+        return dummy.next;
+    }
+};
+
+
+
+
 
 //recursive
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
