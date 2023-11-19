1e7d133ebc02ce17b541ac0dccc18b8cceecef9e
sleepysss <86964370+sleepysss@users.noreply.github.com>
2023-11-19 15:25:33 +0800

    Update 450_Delete Node in a BST.c

--- 450_Delete Node in a BST.c
+++ 450_Delete Node in a BST.c
@@ -63,3 +63,59 @@ struct TreeNode* deleteNode(struct TreeNode* root, int key){
     return root; //return the root node reference of the BST
 
 }
+
+
+
+
+//c++ ver
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+
+        if(!root)
+            return NULL;
+        else if(root->val>key)
+            root->left=deleteNode(root->left,key);
+        else if(root->val<key)
+            root->right=deleteNode(root->right,key);
+        else
+        {
+            if(root->left&&root->right)
+            {
+                int temp = smallest(root->right); //找
+                root->val = temp; //改
+                root->right = deleteNode(root->right,temp); //刪
+                return root;
+            }
+            else
+            {
+                if(root->left)
+                    return root->left;
+                else
+                    return root->right;
+            }   
+
+        }
+        return root;
+    }
+
+    int smallest(TreeNode* root)
+    {
+        while(root->left)
+            root=root->left;
+        return root->val;
+    }
+};
+
