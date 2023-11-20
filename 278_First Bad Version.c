65f54525e35075d2478fa7f6a5a88cc7545de1fc
sleepysss <86964370+sleepysss@users.noreply.github.com>
2023-11-20 16:12:15 +0800

    Update 278_First Bad Version.c

--- 278_First Bad Version.c
+++ 278_First Bad Version.c
@@ -14,3 +14,29 @@ int firstBadVersion(int n) {
     }
     return left;
 }
+
+
+
+//c++ ver
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+
+        int start=1,end=n;
+
+        while(start<=end)
+        {
+            int mid = start+(end-start)/2;
+            bool results = isBadVersion(mid);
+            if(!results)
+                start = mid+1;
+            else
+                end = mid-1;
+        }
+        return start;
+    }
+};
