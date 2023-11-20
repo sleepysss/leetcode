f9bc6b6477df1f52ca58a1044bee5c4174720407
sleepysss <86964370+sleepysss@users.noreply.github.com>
2023-11-20 13:46:05 +0800

    Update 98_Validate Binary Search Tree.c

--- 98_Validate Binary Search Tree.c
+++ 98_Validate Binary Search Tree.c
@@ -116,3 +116,46 @@ bool isValidBST(struct TreeNode* root){
     }
 }
 
+
+
+
+//c++ ver
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void inorder(TreeNode* root, vector<int> &store)
+    {
+        if(!root)
+            return;
+        else
+        {
+            inorder(root->left,store);
+            store.push_back(root->val);
+            inorder(root->right,store);
+        }
+    }
+
+    bool isValidBST(TreeNode* root) {
+        vector<int> store;
+        inorder(root,store);
+        for(int i=0;i<store.size()-1;++i)
+        {
+            if(store[i]>=store[i+1])
+                return false;
+        }
+        return true;
+    }
+};
+
