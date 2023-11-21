83f40e8528f58edcf7c3ccd6c877a57ea6bcb3c8
sleepysss <86964370+sleepysss@users.noreply.github.com>
2023-11-21 15:47:32 +0800

    Update 141_Linked List Cycle.c

--- 141_Linked List Cycle.c
+++ 141_Linked List Cycle.c
@@ -18,3 +18,30 @@ bool hasCycle(struct ListNode *head) {
     }
     return false;  
 }
+
+
+
+//c++ ver
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast=head, *slow=head;
+        while(fast && fast->next && slow)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast==slow) //碰到了
+                return true;    
+        }
+        return false;
+    }
+};
