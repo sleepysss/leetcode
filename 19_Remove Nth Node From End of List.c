61df707b885320b26650fe5c5c5f20c2a9e79435
sleepysss <86964370+sleepysss@users.noreply.github.com>
2023-11-21 16:37:06 +0800

    Update 19_Remove Nth Node From End of List.c

--- 19_Remove Nth Node From End of List.c
+++ 19_Remove Nth Node From End of List.c
@@ -28,3 +28,37 @@ struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
     return dummy.next;
 
 }
+
+
+
+//c++ ver
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode dummy;
+        dummy.next=head;
+        //second:會在被刪的前一個
+        ListNode *first=head, *second=&dummy;
+        for(int i=0;i<n;++i)
+            first=first->next;
+
+        while(first)
+        {
+            first=first->next;
+            second=second->next;
+        }
+        second->next=second->next->next;
+        return dummy.next;
+    }
+};
