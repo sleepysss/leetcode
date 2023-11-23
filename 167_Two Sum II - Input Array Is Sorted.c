f11e06174c02436f9da1bb27628847c845ea5320
sleepysss <86964370+sleepysss@users.noreply.github.com>
2023-11-23 13:50:38 +0800

    Update 167_Two Sum II - Input Array Is Sorted.c

--- 167_Two Sum II - Input Array Is Sorted.c
+++ 167_Two Sum II - Input Array Is Sorted.c
@@ -67,3 +67,32 @@ int* twoSum(int* numbers, int numbersSize, int target, int* returnSize){
     *returnSize=2;
     return store;
 }
+
+
+
+
+//c++ ver
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+
+        int start=0,end=numbers.size()-1;
+        vector<int> ans;
+
+        while(start<=end)
+        {
+            if(numbers[start]+numbers[end]>target)
+                end--;
+            else if(numbers[start]+numbers[end]<target)
+                start++;
+            else
+            {
+                ans.push_back(start+1);
+                ans.push_back(end+1);
+                return ans;
+            }
+        } 
+        return ans;   
+    }
+};
